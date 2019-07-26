cdb551d53920a73451f5f54bb11d24a20456c382
Ricardo Rius <ricardo@parity.io>
2019-07-26 12:32:00 +0200

    Fix Ink compilation

--- shipment_tracking/src/lib.rs
+++ shipment_tracking/src/lib.rs
@@ -1,23 +1,22 @@
 #![cfg_attr(not(any(test, feature = "std")), no_std)]
 
 use ink_core::{
-    env::{println},
     memory::format,
     memory::string::String,
-    storage
+    storage,
 };
 use ink_lang::contract;
 
 
 contract! {
+    #![env = ink_core::env::DefaultSrmlTypes]
+
     struct ShipmentTracking {
         shipped: storage::Value<bool>,
         location: storage::Value<String>,
         temperature: storage::Value<u32>,
         time: storage::Value<u64>
     }
-
-    event Checkpoint { location: storage::Value<String>, temperature: storage::Value<u32>, time: storage::Value<u64> }
 
     impl Deploy for ShipmentTracking {
         fn deploy(&mut self) {
@@ -32,12 +31,11 @@ contract! {
         /// Flips the current state of our smart contract.
         pub(external) fn flip(&mut self) {
             *self.shipped = !*self.shipped;
-            //env.emit(Checkpoint{ location:String::from("Planet"), temperature:0, time:0);
         }
 
         /// Returns the current state.
         pub(external) fn get(&self) -> bool {
-            println(&format!("Storage Value: {:?}", *self.shipped));
+            env.println(&format!("Storage Value: {:?}", *self.shipped));
             *self.shipped
         }
     }
